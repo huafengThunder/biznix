1dad473f77e60497c17c25e0fed9c0b29b91c626
huafengThunder <437832424@qq.com>
2024-12-14 18:36:02 +0800

    config bug

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,28 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.ts', // TypeScript 文件作为入口
+  input: 'src/index.ts',  // 使用 TypeScript 作为入口
   output: [
     {
-      file: 'dist/my-ts-library.cjs.js',  // CommonJS 格式
+      file: 'dist/biznix.cjs.js',  // CommonJS 格式
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
     },
     {
-      file: 'dist/my-ts-library.esm.js',  // ESM 格式
+      file: 'dist/biznix.esm.js',  // ES 模块格式
       format: 'esm',
-      sourcemap: true
+      sourcemap: true,
     },
     {
-      file: 'dist/my-ts-library.umd.js',  // UMD 格式
+      file: 'dist/biznix.umd.js',  // UMD 格式
       format: 'umd',
-      name: 'MyTsLibrary', // 库的全局变量名
-      sourcemap: true
-    }
+      name: 'Biznix',  // UMD 格式的全局变量名
+      sourcemap: true,
+    },
   ],
   plugins: [
-    resolve(), // 解析 `node_modules` 中的模块
-    commonjs(), // 转换 CommonJS 模块为 ES6
-    typescript({ tsconfig: './tsconfig.json' }) // 使用 TypeScript 编译
-  ]
+    resolve(),  // 解析 node_modules 中的模块
+    commonjs(),  // 转换 CommonJS 模块为 ES6
+    typescript({ tsconfig: './tsconfig.json' }),  // 使用 TypeScript 编译
+  ],
 };
