2810fb61fde62caf906aa170a26aa85e91497fce
huafengThunder <437832424@qq.com>
2024-12-16 21:11:18 +0800

    add funtion

--- src/json/is-valid-json.ts
+++ src/json/is-valid-json.ts
@@ -1,13 +1,14 @@
 /**
  * 检查一个字符串是否是有效的 JSON 格式
- * @param {any} str - 要检查的变量
+ * @param {string} str - 要检查的字符串
  * @returns {boolean} - 如果字符串是有效的 JSON 格式，返回 true；否则返回 false
  */
-export function isValidJSON(str: any) {
+export function isValidJSON(str: string): boolean {
+  if (typeof str !== 'string') return false; // 确保输入是字符串
   try {
     JSON.parse(str);
     return true;
-  } catch (e) {
+  } catch {
     return false;
   }
 }
