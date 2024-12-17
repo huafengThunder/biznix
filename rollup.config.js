22929cced5d130ebd66f4b5f13230bf737bb9e80
huafengThunder <437832424@qq.com>
2024-12-17 22:00:54 +0800

    引入路径别名

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,13 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import deletePlugin from 'rollup-plugin-delete';  // 引入 rollup-plugin-delete 插件
 import terser from '@rollup/plugin-terser'; // 压缩插件
+import alias from '@rollup/plugin-alias'; // Import alias plugin
 import path from 'path';
 
-// 获取当前目录的路径
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// 获取当前目录的路径（Rollup 处理路径时可能受到平台（Windows）路径格式的影响。路径多余 E:\E:\ 可能是路径拼接时重复添加了盘符）
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   input: 'src/index.ts',  // 使用 TypeScript 作为入口
@@ -31,11 +34,14 @@ export default {
       plugins: [terser()]
     },
   ],
-  alias: {
-    '@src': path.resolve(__dirname, 'src'),
-  },
+  // 插件执行顺序遵循声明顺序，具体阶段上会有细节区别
   plugins: [
     deletePlugin({ targets: 'dist/*' }),  // 在构建前删除 dist 目录中的所有文件
+    alias({
+      entries: [
+        { find: '@src', replacement: path.resolve(__dirname, 'src') }
+      ]
+    }),
     resolve(),  // 解析 node_modules 中的模块
     commonjs(),  // 转换 CommonJS 模块为 ES6
     // 使用 TypeScript 编译
